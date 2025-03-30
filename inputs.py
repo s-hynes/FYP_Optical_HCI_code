e743e6501e15e6c59ad4f02f908da5e7466b17f4
s-hynes <shynes2@nd.edu>
2025-03-30 23:15:20 +0100

    Added numbers to elements in  save_fits dictionary, removed the indent in this dictionary to make it look slightly nicer.

--- inputs.py
+++ inputs.py
@@ -9,55 +9,55 @@ following objects:
 
 **save_fits**   - A dictionary used to specify which FITS files for intermediate steps should be saved"""
 
-dir_in_str = "C:/Users/Stephen/OneDrive - National University of Ireland, Galway/24-25 FYP/Data/GG-Tau-data"
-saving_dir = "C:/Users/Stephen/OneDrive - National University of Ireland, Galway/24-25 FYP/Data/manipulated-data/GG-Tau/steps_presentation"
+dir_in_str = "C:/Users/Stephen/OneDrive - National University of Ireland, Galway/24-25 FYP/Data/ESO-Test-data"
+saving_dir = "C:/Users/Stephen/OneDrive - National University of Ireland, Galway/24-25 FYP/Data/manipulated-data/01_combining_detectors"
 save_steps = True
 
 save_fits = {
-    "0 phase mode": 0,                                                      # 0
-    "pi phase mode": 0,                                                     # 2
-    "0 overscan removed": 0,                                                # 3
-    "pi overscan removed": 0,                                               # 4
-    "0 ordinary ray": 0,                                                    # 5
-    "0 extraordinary ray": 0,                                               # 6
-    "pi ordinary ray": 0,                                                   # 7
-    "pi extraordinary ray": 0,
-    "0 ordinary ray, star centered": 0,
-    "0 extraordinary ray, star centered": 0,
-    "pi ordinary ray, star centered": 0,
-    "pi extraordinary ray, star centered": 0,
-    "single difference 1024x512": 0,
-    "intensity 1024x512": 0,
-    "single difference 512x512": 0,
-    "intensity 512x512": 0,
-    "Qplus": 1,
-    "Qminus": 1,
-    "Uplus": 1,
-    "Uminus": 1,
-    "Qplus intensity": 1,
-    "Qminus intensity": 1,
-    "Uplus intensity": 1,
-    "Uminus intensity": 1,
-    "Q double difference": 1,
-    "U double difference": 1,
-    "Q intensity": 1,
-    "U intensity": 1,
-    "total intensity": 1,
-    "polarised intensity": 1,
-    "Q double difference average": 1,
-    "U double difference average": 1,
-    "Q intensity average": 1,
-    "U intensity average": 1,
-    "total intensity average": 1,
-    "polarised intensity average": 1,
-    "Q double difference corrected for instrumental polarisation": 1,
-    "U double difference corrected for instrumental polarisation": 1,
-    "polarised intensity corrected for instrumental polarisation": 1,
-    "Qphi (azimuthal Q)": 1,
-    "Uphi (azimuthal U)": 1,
-    "Q double difference, average of two detectors": 1,
-    "U double difference, average of two detectors": 1,
-    "polarised intensity, average of two detectors": 1,
-    "Qphi, average of two detectors": 1,
-    "Uphi, average of two detectors": 1
-    }
+"0 phase mode": 0,                                                      #   1
+"pi phase mode": 0,                                                     #   2
+"0 overscan removed": 0,                                                #   3
+"pi overscan removed": 0,                                               #   4
+"0 ordinary ray": 0,                                                    #   5
+"0 extraordinary ray": 0,                                               #   6
+"pi ordinary ray": 0,                                                   #   7
+"pi extraordinary ray": 0,                                              #   8
+"0 ordinary ray, star centered": 0,                                     #   9
+"0 extraordinary ray, star centered": 0,                                #   10
+"pi ordinary ray, star centered": 0,                                    #   11
+"pi extraordinary ray, star centered": 0,                               #   12
+"single difference 1024x512": 0,                                        #   13
+"intensity 1024x512": 0,                                                #   14
+"single difference 512x512": 0,                                         #   15
+"intensity 512x512": 0,                                                 #   16
+"Qplus": 1,                                                             #   17
+"Qminus": 1,                                                            #   18
+"Uplus": 1,                                                             #   19
+"Uminus": 1,                                                            #   20
+"Qplus intensity": 1,                                                   #   21
+"Qminus intensity": 1,                                                  #   22
+"Uplus intensity": 1,                                                   #   23
+"Uminus intensity": 1,                                                  #   24
+"Q double difference": 1,                                               #   25
+"U double difference": 1,                                               #   26
+"Q intensity": 1,                                                       #   27
+"U intensity": 1,                                                       #   28
+"total intensity": 1,                                                   #   29
+"polarised intensity": 1,                                               #   30
+"Q double difference average": 1,                                       #   31
+"U double difference average": 1,                                       #   32
+"Q intensity average": 1,                                               #   33
+"U intensity average": 1,                                               #   34
+"total intensity average": 1,                                           #   35
+"polarised intensity average": 1,                                       #   36
+"Q double difference corrected for instrumental polarisation": 1,       #   37
+"U double difference corrected for instrumental polarisation": 1,       #   38
+"polarised intensity corrected for instrumental polarisation": 1,       #   39
+"Qphi (azimuthal Q)": 1,                                                #   40
+"Uphi (azimuthal U)": 1,                                                #   41
+"Q double difference, average of two detectors": 1,                     #   42
+"U double difference, average of two detectors": 1,                     #   43
+"polarised intensity, average of two detectors": 1,                     #   44
+"Qphi, average of two detectors": 1,                                    #   45
+"Uphi, average of two detectors": 1                                     #   46
+}
